bfad2e5a15fb074c43a317c8438abbbbc1df92d9
Kimbelly Nguyen <Kimchi@Kimbellys-MacBook-Pro.local>
2021-01-13 15:54:10 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function show(x) {
+    x = document.getElementById("hidden");
+    x.style.display = "block";
+  };
+  
+  function hide() {
+    x = document.getElementById("hidden");
+    x.style.display = "none";
+  };
